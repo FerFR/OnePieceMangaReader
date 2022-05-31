ccbecc05afefeae70b5f348dc2b3dd367d803305
FernandoFR <fernandofr077@gmail.com>
2022-05-31 12:16:20 -0300

    add back button

--- src/components/Reader.jsx
+++ src/components/Reader.jsx
@@ -1,32 +1,52 @@
 import React from 'react';
 import styled from 'styled-components';
-
 const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+`;
+const Content = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
     flex-direction: column;
+    img.current {
+        display: block;
+    }
 `;
 const Image = styled.img`
-    display: ${(props) => (props.active ? 'block' : 'none')};
+    display: none;
     object-fit: contain;
     cursor: pointer;
     width: 100%;
 `;
 
+const BackButton = styled.button`
+    margin-right: auto;
+    border-radius: 4px;
+    padding: 4px 8px;
+    font-size: large;
+    background-color: #da2d58;
+    border: none;
+    cursor: pointer;
+    transition: 100ms;
+    :hover {
+        background-color: #ac3754;
+    }
+`;
 const Reader = () => {
     const hideImage = (e) => {
-        e.target.style.display = 'none';
+        e.target.classList.remove('current');
     };
 
     const checkIfExistsNextImage = (e) => {
         return Boolean(e.target.nextElementSibling);
     };
     const showNextImage = (e) => {
-        e.target.nextElementSibling.style.display = 'block';
+        e.target.nextElementSibling.classList.add('current');
     };
     const showFirstImage = (e) => {
-        document.getElementById('first').style.display = 'block';
+        document.getElementById('first').classList.add('current');
     };
     const scrollTop = () => {
         window.scroll(0, 0);
@@ -40,30 +60,44 @@ const Reader = () => {
             alert('Capítulo chegou ao fim!');
             showFirstImage(e);
         }
+
         scrollTop();
+    };
+
+    const backImage = () => {
+        const actualImage = document.getElementsByClassName('current')[0];
+        if (!actualImage.previousElementSibling) {
+            return;
+        }
+        actualImage.classList.remove('current');
+        actualImage.previousElementSibling.classList.add('current');
     };
     return (
         <Container>
-            <Image
-                id="first"
-                onClick={handleClick}
-                src="manga/01.jpg"
-                active={true}
-            />
-            <Image onClick={handleClick} src="manga/02.webp" />
-            <Image onClick={handleClick} src="manga/03.webp" />
-            <Image onClick={handleClick} src="manga/04.webp" />
-            <Image onClick={handleClick} src="manga/05.webp" />
-            <Image onClick={handleClick} src="manga/06.webp" />
-            <Image onClick={handleClick} src="manga/07.webp" />
-            <Image onClick={handleClick} src="manga/08.webp" />
-            <Image onClick={handleClick} src="manga/09.webp" />
-            <Image onClick={handleClick} src="manga/10.webp" />
-            <Image onClick={handleClick} src="manga/11.webp" />
-            <Image onClick={handleClick} src="manga/12.webp" />
-            <Image onClick={handleClick} src="manga/13.webp" />
-            <Image onClick={handleClick} src="manga/14.webp" />
-            <Image onClick={handleClick} src="manga/15.webp" />
+            <BackButton onClick={backImage}>Voltar</BackButton>
+            <Content>
+                <Image
+                    id="first"
+                    onClick={handleClick}
+                    src="manga/01.jpg"
+                    active={true}
+                    className="current"
+                />
+                <Image onClick={handleClick} src="manga/02.webp" />
+                <Image onClick={handleClick} src="manga/03.webp" />
+                <Image onClick={handleClick} src="manga/04.webp" />
+                <Image onClick={handleClick} src="manga/05.webp" />
+                <Image onClick={handleClick} src="manga/06.webp" />
+                <Image onClick={handleClick} src="manga/07.webp" />
+                <Image onClick={handleClick} src="manga/08.webp" />
+                <Image onClick={handleClick} src="manga/09.webp" />
+                <Image onClick={handleClick} src="manga/10.webp" />
+                <Image onClick={handleClick} src="manga/11.webp" />
+                <Image onClick={handleClick} src="manga/12.webp" />
+                <Image onClick={handleClick} src="manga/13.webp" />
+                <Image onClick={handleClick} src="manga/14.webp" />
+                <Image onClick={handleClick} src="manga/15.webp" />
+            </Content>
         </Container>
     );
 };
